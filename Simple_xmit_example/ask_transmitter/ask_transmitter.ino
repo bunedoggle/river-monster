23a877693cd3f93e92b3c92ac47ce5754d5ee047
Brian Bailey <brian@bunedoggle.com>
2014-11-05 10:53:43 -0500

    Pushing a working xmit/rcv example that connects two arduinos via cheap 433mhz modules

---
+++ Simple_xmit_example/ask_transmitter/ask_transmitter.ino
@@ -0,0 +1,28 @@
+// ask_transmitter.pde
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to transmit messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) transmitter with an TX-C1 module
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+//RH_ASK::RH_ASK(uint16_t speed, uint8_t rxPin, uint8_t txPin, uint8_t pttPin, bool pttInverted)
+//RH_ASK driver( 1200, 2, 3, 4, 5 );
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);	  // Debugging only
+    if (!driver.init() )
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    const char *msg = "hello";
+
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(200);
+}
